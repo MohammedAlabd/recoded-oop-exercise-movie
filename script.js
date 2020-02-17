be91fd4c9e184e69e9a0b75941cd0beaf8b7e816
Mohammed <github email address>
2020-02-17 14:22:00 +0300

    add Actor class and render the first 4 actors

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185'
-
+const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
+let urlB 
 class App {
   static run() {
-    APIService.fetchMovie(534)
+    APIService.fetchMovie(634)
       .then(movie => Page.renderMovie(movie))
+
+    APIService.fetchActors(634)
+      .then(array =>array.forEach(obj=>Page.renderActors(obj)))
   }
 }
 
 class APIService {
-  static BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
 
   static fetchMovie(movieId) {
     const url = APIService._constructUrl(`movie/${movieId}`)
@@ -19,7 +22,21 @@ class APIService {
   }
 
   static  _constructUrl(path) {
-    return `${APIService.BACKDROP_BASE_URL}/${path}?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}`
+    return `${TMDB_BASE_URL}/${path}?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}`
+  }
+
+  static fetchActors(movieId){
+    const url = APIService._constructUrl(`movie/${movieId}/credits`)
+    return fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        let actorsArray = []
+        for(let i = 0; i <= 3; i++){
+          let actor = new Actor(json.cast[i])
+          actorsArray.push(actor)
+        }
+        return actorsArray
+      })
   }
 }
 
@@ -36,6 +53,26 @@ class Page {
     Page.releaseDate.innerText = movie.releaseDate
     Page.runtime.innerText = movie.runtime + " minutes"
     Page.overview.innerText = movie.overview
+  }
+
+  static renderActors (actor) {
+    const card = document.createElement("li")
+    document.querySelector(".list-unstyled").appendChild(card)
+    card.setAttribute("class","card")
+    card.setAttribute("class","col-md-3")
+    card.insertAdjacentHTML('beforeend',' <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title"> </h5> <h3 class="card-text"></h3> </div> ');
+    card.querySelector(".card-img-top").setAttribute("src",PROFILE_BASE_URL + actor.picPath)
+    card.querySelector(".card-title").innerHTML = actor.name
+    card.querySelector(".card-text").innerHTML = actor.character
+  }
+}
+
+class Actor {
+  constructor(json){
+    this.id = json.id
+    this.name = json.name
+    this.character = json.character
+    this.picPath = json.profile_path
   }
 }
 
