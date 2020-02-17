8a8a7d8728fd5cadd3caa6f95d497df4fa952cf2
Mohammed <github email address>
2020-02-17 21:22:59 +0300

    make it more readable

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185'
 const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
-let urlB 
 class App {
   static run() {
     APIService.fetchMovie(634)
       .then(movie => Page.renderMovie(movie))
 
     APIService.fetchActors(634)
-      .then(array =>array.forEach(obj=>Page.renderActors(obj)))
+      .then(actors => Page.renderActor(actors))
   }
 }
 
@@ -31,7 +30,7 @@ class APIService {
       .then(res => res.json())
       .then(json => {
         let actorsArray = []
-        for(let i = 0; i <= 3; i++){
+        for(let i = 0; i <= 3 && i<json.cast.length; i++){
           let actor = new Actor(json.cast[i])
           actorsArray.push(actor)
         }
@@ -46,6 +45,7 @@ class Page {
   static releaseDate = document.getElementById('movie-release-date')
   static runtime = document.getElementById('movie-runtime')
   static overview = document.getElementById('movie-overview')
+  static actors = document.querySelector("#actors")
 
   static renderMovie(movie) {
     Page.backdrop.src = BACKDROP_BASE_URL + movie.backdropPath
@@ -55,15 +55,18 @@ class Page {
     Page.overview.innerText = movie.overview
   }
 
-  static renderActors (actor) {
-    const card = document.createElement("li")
-    document.querySelector(".list-unstyled").appendChild(card)
-    card.setAttribute("class","card")
-    card.setAttribute("class","col-md-3")
-    card.insertAdjacentHTML('beforeend',' <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title"> </h5> <h3 class="card-text"></h3> </div> ');
-    card.querySelector(".card-img-top").setAttribute("src",PROFILE_BASE_URL + actor.picPath)
-    card.querySelector(".card-title").innerHTML = actor.name
-    card.querySelector(".card-text").innerHTML = actor.character
+  static renderActor (actors) {
+    actors.forEach(actor => {
+      Page.actors.insertAdjacentHTML("beforeend",`
+        <li class = "card col-md-3">
+            <img src="${actor.profilePictureURL}" class="card-img-top" alt="${actor.id}">
+            <div class="card-body">
+              <h5 class="card-title">${actor.name}</h5>
+              <h3 class="card-text">${actor.character}</h3> 
+            </div>
+        </li>
+      `)
+    })    
   }
 }
 
@@ -73,6 +76,10 @@ class Actor {
     this.name = json.name
     this.character = json.character
     this.picPath = json.profile_path
+  }
+
+  get profilePictureURL() {
+    return PROFILE_BASE_URL + this.picPath
   }
 }
 
